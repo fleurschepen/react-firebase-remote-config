3b0d85583506a5a77accbda5ddbac1952ef080b7
Fleur Schepen <26626361+fleurschepen@users.noreply.github.com>
2020-01-25 17:27:06 +0000

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import * as firebaseApp from 'firebase/app';
+import 'firebase/remote-config';
+
+const firebaseConfig = {
+	apiKey: "api-key",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "sender-id",
+  appId: "app-id",
+  measurementId: "G-measurement-id",
+};
+
+export const firebase = firebaseApp.initializeApp(firebaseConfig);
