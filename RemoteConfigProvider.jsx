c35ad1e770342ef77478e0aafe8b667242f3d43d
Fleur Schepen <26626361+fleurschepen@users.noreply.github.com>
2020-01-25 17:33:24 +0000

    Update RemoteConfigProvider.jsx

--- RemoteConfigProvider.jsx
+++ RemoteConfigProvider.jsx
@@ -4,25 +4,25 @@ import activateRemoteConfig from './remote-config';
 export const RemoteConfigContext = createContext({});
 
 export default function RemoteConfigProvider({ settings, defaultConfig, children }) {
-	const [remoteConfigParameters, setRemoteConfigParameters] = useState(undefined);
+  const [remoteConfigParameters, setRemoteConfigParameters] = useState(undefined);
 
-	useEffect(() => {
-		Promise.resolve(activateRemoteConfig(settings, defaultConfig))
-			.then(res => {
-				setRemoteConfigParameters(res);
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	}, [settings, defaultConfig]);
+  useEffect(() => {
+    Promise.resolve(activateRemoteConfig(settings, defaultConfig))
+      .then(res => {
+        setRemoteConfigParameters(res);
+      })
+    .catch(err => {
+      console.log(err);
+    });
+  }, [settings, defaultConfig]);
 
-	return (
-		<RemoteConfigContext.Provider
-			value={{
-				remoteConfigParameters: remoteConfigParameters,
-			}}
-		>
-			{remoteConfigParameters !== undefined && children}
-		</RemoteConfigContext.Provider>
-	);
+  return (
+    <RemoteConfigContext.Provider
+      value={{
+        remoteConfigParameters: remoteConfigParameters,
+      }}
+    >
+      {remoteConfigParameters !== undefined && children}
+    </RemoteConfigContext.Provider>
+  );
 }
