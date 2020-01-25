456ffe274e060f9eb805870b83a653528aa5badd
Fleur Schepen <26626361+fleurschepen@users.noreply.github.com>
2020-01-25 17:34:50 +0000

    Update withRemoteConfig.jsx

--- withRemoteConfig.jsx
+++ withRemoteConfig.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { RemoteConfigContext } from './RemoteConfigProvider';
 
 export function withRemoteConfig(Component) {
-	return function WrapperComponent(props) {
-		return (
-			<RemoteConfigContext.Consumer>
-				{({ remoteConfigParameters }) => <Component {...props} {...remoteConfigParameters} />}
-			</RemoteConfigContext.Consumer>
-		);
-	};
+  return function WrapperComponent(props) {
+    return (
+      <RemoteConfigContext.Consumer>
+        {({ remoteConfigParameters }) => <Component {...props} {...remoteConfigParameters} />}
+      </RemoteConfigContext.Consumer>
+    );
+  };
 }
